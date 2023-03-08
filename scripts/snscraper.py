0f615adf767f76ed4862ce84dffd93485dfec6ab
hesamr776 <h.rajaei@wallex.net>
2023-03-08 16:10:15 +0000

    feat: script: snscraper

---
+++ scripts/snscraper.py
@@ -0,0 +1,53 @@
+import sys
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+import datetime
+
+[username, since] = sys.argv[1:]
+
+
+def search(text, username, since, until, retweet, replies):
+    q = text
+
+    if username != '':
+        q += f" from:{username}"
+
+    if until == '':
+        until = datetime.datetime.strftime(datetime.datetime.today(), '%Y-%m-%d')
+    q += f" until:{until}"
+    if since == '':
+        since = datetime.datetime.strftime(datetime.datetime.strptime(until, '%Y-%m-%d') - datetime.timedelta(days=7)
+                                           , '%Y-%m-%d')
+    q += f" since:{since}"
+
+    if retweet == 'y':
+        q += f" exclude:retweets"
+    if replies == 'y':
+        q += f" exclude:replies"
+
+    # FILE NAME
+    if username != '' and text != '':
+        filename = f"{since}_{until}_{username}_{text}.csv"
+    elif username != "":
+        filename = f"{since}_{until}_{username}.csv"
+    else:
+        filename = f"{since}_{until}_{text}.csv"
+    return q
+
+# Created a list to append all tweet attributes(data)
+attributes_container = []
+
+query = search('', str(username), str(since), '', 'y', 'y')
+
+# Using TwitterSearchScraper to scrape data and append tweets to list
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
+    if tweet.inReplyToTweetId is None:
+        attributes_container.append([tweet.date, tweet.id, tweet.rawContent, tweet.user.username, tweet.replyCount,
+                                     tweet.retweetCount, tweet.likeCount, tweet.quoteCount, tweet.inReplyToTweetId])
+
+# Creating a dataframe from the tweets list above
+tweets_df = pd.DataFrame(attributes_container, columns=['DateTime', 'TweetId', 'Text', 'Username', 'ReplyCount',
+                                                        'RetweetCount', 'LikeCount', 'QuoteCount', '-'])
+
+# Creating a dataframe from the tweets list above
+print(attributes_container)
