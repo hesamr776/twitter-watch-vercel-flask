d4bfa9f18f894bdb1d379e66a0042c6c0220f7b2
hesamr776 <h.rajaei@wallex.net>
2023-03-08 16:09:30 +0000

    feat: api: def

--- api/index.py
+++ api/index.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/accounts')
+def accounts():
+    return [{ "name": "Elon Musk", "avatar": "", "background": "" }]
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/audience')
+def audience():
+    return 'audience'
+
+@app.route('/sentiment')
+def sentiment():
+    return 'sentiment'
+    
+@app.route('/tweets')
+def tweets():
+    return 'tweets'
