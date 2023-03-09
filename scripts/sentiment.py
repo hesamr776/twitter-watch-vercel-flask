aac8d0431ca818660e6bd249ecb25f419fce3ffb
farazML <faraz.fatahnaie@gmail.com>
2023-03-09 15:27:24 +0330

    sentiment.py

--- scripts/sentiment.py
+++ scripts/sentiment.py
@@ -1,2 +1,55 @@
-import transformers
+
 import torch
+
+import random
+import numpy as np
+from transformers import BertTokenizer
+from torchtext.legacy import data
+from torchtext.legacy import datasets
+
+SEED = 1234
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+len(tokenizer.vocab)
+
+tokens = tokenizer.tokenize('Hello WORLD how ARE yoU?')
+
+print(tokens)
+
+indexes = tokenizer.convert_tokens_to_ids(tokens)
+
+print(indexes)
+
+init_token_idx = tokenizer.cls_token_id
+eos_token_idx = tokenizer.sep_token_id
+pad_token_idx = tokenizer.pad_token_id
+unk_token_idx = tokenizer.unk_token_id
+
+print(init_token_idx, eos_token_idx, pad_token_idx, unk_token_idx)
+
+
+def tokenize_and_cut(sentence):
+    tokens = tokenizer.tokenize(sentence)
+    tokens = tokens[:max_input_length - 2]
+    return tokens
+
+
+TEXT = data.Field(batch_first=True,
+                  use_vocab=False,
+                  tokenize=tokenize_and_cut,
+                  preprocessing=tokenizer.convert_tokens_to_ids,
+                  init_token=init_token_idx,
+                  eos_token=eos_token_idx,
+                  pad_token=pad_token_idx,
+                  unk_token=unk_token_idx)
+
+LABEL = data.LabelField(dtype=torch.float)
+
+train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+
+train_data, valid_data = train_data.split(random_state = random.seed(SEED))
